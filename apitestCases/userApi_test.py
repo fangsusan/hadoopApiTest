65e2ea12ebdc3f22d6cb528c581f194997216ad3
fangsusan <yuanjing-fang@163.com>
2021-05-12 14:32:36 +0800

    2021-2-12 用户管理api补写，及批量接口调通

--- apitestCases/userApi_test.py
+++ apitestCases/userApi_test.py
@@ -1,28 +1,41 @@
 import pytest
-
+import allure
 from pageApi.userPage import userPage
 
-
+@allure.feature('Witserver 用户管理接口模块 测试')
 class Testuser:
     """ WitServer 用户管理接口  测试"""
     def setup(self):
         self.userPage = userPage()
+        self.userId = 46914125908955136
+        self.roleId = 46923964773060608
+        self.roleIds = [46923964773060608,46958389690912768]
 
     def teardown(self):
-        pass
+        self.userPage.batchDeleteRole(userId=self.userId, roleIds=self.roleIds)
 
+    def test_getUsers(self):
+        """ 获取所有用户 正例"""
+        result = self.userPage.getUsers()
+        assert result['status'] == 200
+        assert result['data'] != 0
+
+    @allure.story('添加用户信息 正例')
     @pytest.mark.parametrize("password,salt,username,pageSize,pageNum",[("123789","salt","fyj001",1,10)])
     def test_insert(self,password,salt,username,pageSize,pageNum):
         """ 添加用户信息 正例"""
-
-        result = self.userPage.insert(password=password,salt=salt,username=username)
+        with allure.step('添加用户信息'):
+            result = self.userPage.insert(password=password,salt=salt,username=username)
         id = result['data']['id']
-        try:
-            self.userPage.findByName(name=username,pageSize=pageSize,pageNum=pageNum)
-        finally:
-            self.userPage.deleteUser(id=id)
-        assert result['status'] == 200
+        with allure.step('检查是否存在，并删除'):
 
+            try:
+                self.userPage.findByName(name=username,pageSize=pageSize,pageNum=pageNum)
+            finally:
+                self.userPage.deleteUser(id=id)
+        with allure.step("校验结果"):
+            allure.attach('期望结果', '验证通过')
+            assert result['status'] == 200
 
     def test_countAll(self):
         """ 获取用户总数 正例"""
@@ -30,7 +43,7 @@ class Testuser:
         assert result['status'] == 200
         assert result['data'] != 0
 
-    @pytest.mark.parametrize("name",[("meilanzi")])
+    @pytest.mark.parametrize("name",[("18768466309")])
     def test_countByName(self,name):
         """ 根据用户名称获取用户总数 正例"""
         result = self.userPage.countByName(name=name)
@@ -45,6 +58,14 @@ class Testuser:
         print(result)
         assert result['status'] == 200
         assert result['data']['name'] ==name
+
+    @pytest.mark.parametrize("userId",[(46914125908955136)])
+    def test_getOrganizations(self,userId):
+        """ 获取用户所有组织 正例"""
+        result = self.userPage.getOrganizations(userId=userId)
+        print(result)
+        assert result['status'] == 200
+        assert result['data'] != 0
 
     @pytest.mark.parametrize("pageSize,pageNum",[(1,10)])
     def test_findPage(self,pageSize, pageNum):
@@ -81,10 +102,50 @@ class Testuser:
         result = self.userPage.deleteUser(id=id)
         assert result['status'] == 200
 
+    @pytest.mark.parametrize("userId",[(46914125908955136)])
+    def test_getPermissions(self,userId):
+        """获取用户所有权限 正例"""
+        result = self.userPage.getPermissions(userId=userId)
+        assert result['status'] == 200
+
+    @pytest.mark.parametrize("userId",[(46914125908955136)])
+    def test_getRoles(self,userId):
+        """获取用户所有角色 正例"""
+        result = self.userPage.getRoles(userId=userId)
+        assert result['status'] == 200
+
+    def test_batchInsertRole(self):
+        """批量添加用户角色 正例"""
+        result = self.userPage.batchInsertRole(userId=self.userId,roleIds=self.roleIds)
+        assert result['status'] == 200
 
 
-#     批量操作待写
+    def test_batchDeleteRole(self):
+        """ 删除用户下的一批角色  正例"""
+        result = self.userPage.batchDeleteRole(userId=self.userId,roleIds=self.roleIds)
+        assert result['status'] == 200
 
+    def test_deleteAllRole(self):
+        """ 删除用户下的所有角色 正例 """
+        try:
+            self.userPage.batchInsertRole(userId=self.userId,roleIds=self.roleIds)
+        finally:
+            result = self.userPage.deleteAllRole(userId=self.userId)
+        assert result['status'] == 200
+
+    def test_insertRole(self):
+        """ 添加一个用户角色 正例"""
+        result = self.userPage.insertRole(userId=self.userId,roleId=self.roleId)
+        print(result)
+        assert result['status'] == 200
+
+    def test_deleteRole(self):
+        """删除用户下的某个角色 正例"""
+        try:
+            self.userPage.insertRole(userId=self.userId,roleId=self.roleId)
+        finally:
+            result = self.userPage.deleteRole(userId=self.userId,roleId=self.roleId)
+        assert result['status'] == 200
 
 
 
--- pageApi/userPage.py
+++ pageApi/userPage.py
@@ -6,6 +6,11 @@ from Api import Api
 class userPage(Api):
     """ WitServer 用户管理接口"""
 
+    def getUsers(self):
+        """获取所有用户 """
+        r = requests.get(f"{self.url()}/api/user")
+        return r.json()
+
     def insert(self,password,salt,username,**kwargs):
         """添加用户信息"""
         data = {
@@ -32,6 +37,11 @@ class userPage(Api):
         r = requests.get(f"{self.url()}/api/user/name/{name}/{pageSize}/{pageNum}")
         return r.json()
 
+    def getOrganizations(self,userId):
+        """获取用户所有组织"""
+        r = requests.get(f"{self.url()}/api/user/organizations/{userId}")
+        return r.json()
+
     def findPage(self,pageNum,pageSize):
         """获取用户列表"""
         r = requests.get(f"{self.url()}/api/user/page/{pageSize}/{pageNum}")
@@ -56,26 +66,25 @@ class userPage(Api):
         r =requests.delete(f"{self.url()}/api/user/{id}")
         return r.json()
 
-    def delete(self,userId,password,salt,username,**kwargs):
-        """批量添加用户角色"""
-        data = {
-            "password": password,
-            "salt": salt,
-            "username": username,
-        }
-        data.update(kwargs)
-        r = requests.put(f"{self.url()}/api/user/{userId}/user",json=data)
+    def getPermissions(self,userId):
+        """获取用户所有权限 """
+        r = requests.get(f"{self.url()}/api/user/{userId}/permission")
+        return r.json()
+    def getRoles(self,userId):
+        """获取用户所有角色 """
+        r = requests.get(f"{self.url()}/api/user/{userId}/role")
         return r.json()
 
-    def batchInsertRole(self,roleId):
+    def batchInsertRole(self,userId,roleIds):
         """批量添加用户角色"""
-        r = requests.put(f"{self.url()}/api/user/{roleId}/user")
+        data = roleIds
+        r = requests.put(f"{self.url()}/api/user/{userId}/user",json=data)
         return  r.json()
-
 
     def batchDeleteRole(self,userId,roleIds):
         """删除用户下的一批角色"""
-        r = requests.post(f"{self.url()}/api/user/{userId}/role")
+        data = roleIds
+        r = requests.post(f"{self.url()}/api/user/{userId}/role",json=data)
         return r.json()
 
     def deleteAllRole(self,userId):
@@ -86,7 +95,7 @@ class userPage(Api):
 
     def insertRole(self,userId,roleId):
         """添加一个用户角色"""
-        r = requests.put(f"{self.url()}/api/user/{userId}/user/{roleId}")
+        r = requests.put(f"{self.url()}/api/user/{userId}/role/{roleId}")
         return r.json()
 
     def deleteRole(self,userId,roleId):
