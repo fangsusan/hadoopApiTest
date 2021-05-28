306c29ca7ef0540bb46579f9da2fae59a262f5b9
fangsusan <73986524+fangsusan@users.noreply.github.com>
2021-05-28 17:20:20 +0800

    2021-5-28 用例代码完善

--- apitestCases/userApi_test.py
+++ apitestCases/userApi_test.py
@@ -8,14 +8,14 @@ from common.randomUserinfo import random_name, random_phone, random_password
 class Testuser:
     """ WitServer 用户管理接口  测试"""
 
-    def setup(self):
+    def setup_class(self):
         self.userPage = userPage()
         self.userId = 46914125908955136
         self.roleId = 46923964773060608
         self.roleIds = [46923964773060608,46958389690912768]
 
-    def teardown(self):
-        self.userPage.batchDeleteRole(userId=self.userId, roleIds=self.roleIds)
+    def teardown_class(self):
+        self.userPage.batchDeleteRole(self.userId, self.roleIds)
 
     @pytest.mark.parametrize("username,password",[("fangy",111111)])
     def test_login(self,username,password):
@@ -34,12 +34,7 @@ class Testuser:
     @allure.story('添加用户信息 正例')
     def test_insert(self):
         """ 添加用户信息admin登录 正例"""
-        name = random_name(8)
-        phone = random_phone(11)
-        email = "2222@qq.com"
-        password = random_password()
-        username = random_name(9)
-        result = self.userPage.insert(name=name,phone=phone, email=email, password=password, username=username)
+        result = self.userPage.insert(name=random_name(8),phone=random_phone(11), email="2222@qq.com", password=random_password(6), username=random_name(9))
         print(result)
         assert result['status'] == 200
 
@@ -72,7 +67,7 @@ class Testuser:
     @pytest.mark.parametrize("userId",[(46914125908955136)])
     def test_getOrganizations(self,userId):
         """ 获取用户所有组织 正例"""
-        result = self.userPage.getOrganizations(userId=userId)
+        result = self.userPage.getOrganizations(userId)
         print(result)
         assert result['status'] == 200
         assert result['data'] != 0
@@ -82,7 +77,7 @@ class Testuser:
     @pytest.mark.parametrize("pageSize,pageNum",[(1,10)])
     def test_findPage(self,pageSize, pageNum):
         """ 获取用户列表 正例"""
-        result = self.userPage.findPage(pageSize=pageSize, pageNum=pageNum)
+        result = self.userPage.findPage(pageSize,pageNum)
         print(result)
         assert result['status'] == 200
 
@@ -90,7 +85,7 @@ class Testuser:
     def test_changePassword(self):
         """ 修改密码 用户修改密码"""
         try:
-            result = self.userPage.changePwd(username="fangy",password=random_password())
+            result = self.userPage.changePwd(username="fangy",password=random_password(7))
         finally:
             self.userPage.adminchangePwd(username="fangy",password="111111")
         print(result)
@@ -114,11 +109,10 @@ class Testuser:
     # @pytest.mark.parametrize("id,realname,phone,email", [("50527843863515136","fangj",18768466308,"123456789@qq.com")])
     def test_updateUser(self):
         """ 更新用户信息 正例"""
-        realname = random_name(8)
-        phone = random_phone(11)
-        email = "11111@qq.com"
-        id = 50527843863515136
-        result = self.userPage.updateUser(id=id,realname=realname,phone=phone,email=email)
+        pre = self.userPage.insert(name=random_name(9),phone=random_phone(11),email="1234@qq.com",username=random_name(11),password=123456)
+        print(pre)
+        id = pre['data']['id']
+        result = self.userPage.updateUser(id=id,realname=random_name(8),phone=random_phone(11),email="11111@qq.com")
         print(result)
         assert result['status'] == 200
 
@@ -126,7 +120,7 @@ class Testuser:
     @pytest.mark.parametrize("id,status",[(50534537125449728,0),(50534537125449728,1)])
     def test_changeUserStatus(self,id,status):
         """ status：0禁用/ 1 启用用户 正例"""
-        result = self.userPage.changeUserStatus(id=id,status=status)
+        result = self.userPage.changeUserStatus(id,status)
         print(result)
         assert result['status'] == 200
 
@@ -134,42 +128,41 @@ class Testuser:
     @pytest.mark.parametrize("userId",[(46914125908955136)])
     def test_getPermissions(self,userId):
         """获取用户所有权限 正例"""
-        result = self.userPage.getPermissions(userId=userId)
+        result = self.userPage.getPermissions(userId)
         assert result['status'] == 200
 
     @allure.story("获取用户所有角色 正例")
     @pytest.mark.parametrize("userId",[(46914125908955136)])
     def test_getRoles(self,userId):
         """获取用户所有角色 正例"""
-        result = self.userPage.getRoles(userId=userId)
+        result = self.userPage.getRoles(userId)
         assert result['status'] == 200
 
     @allure.story("批量添加用户角色 正例")
     def test_batchInsertRole(self):
         """批量添加用户角色 正例"""
-        result = self.userPage.batchInsertRole(userId=self.userId,roleIds=self.roleIds)
+        result = self.userPage.batchInsertRole(self.userId,self.roleIds)
         assert result['status'] == 200
 
     @allure.story("删除用户下的一批角色 正例")
     def test_batchDeleteRole(self):
         """ 删除用户下的一批角色  正例"""
-        result = self.userPage.batchDeleteRole(userId=self.userId,roleIds=self.roleIds)
+        result = self.userPage.batchDeleteRole(self.userId,self.roleIds)
         assert result['status'] == 200
-
 
     @allure.story("删除用户下的所有角色 正例")
     def test_deleteAllRole(self):
         """ 删除用户下的所有角色 正例 """
         try:
-            self.userPage.batchInsertRole(userId=self.userId,roleIds=self.roleIds)
+            self.userPage.batchInsertRole(self.userId,self.roleIds)
         finally:
-            result = self.userPage.deleteAllRole(userId=self.userId)
+            result = self.userPage.deleteAllRole(self.userId)
         assert result['status'] == 200
 
     @allure.story("添加一个用户角色 正例")
     def test_insertRole(self):
         """ 添加一个用户角色 正例"""
-        result = self.userPage.insertRole(userId=self.userId,roleId=self.roleId)
+        result = self.userPage.insertRole(self.userId,self.roleId)
         print(result)
         assert result['status'] == 200
 
@@ -177,9 +170,9 @@ class Testuser:
     def test_deleteRole(self):
         """删除用户下的某个角色 正例"""
         try:
-            self.userPage.insertRole(userId=self.userId,roleId=self.roleId)
+            self.userPage.insertRole(self.userId,self.roleId)
         finally:
-            result = self.userPage.deleteRole(userId=self.userId,roleId=self.roleId)
+            result = self.userPage.deleteRole(self.userId,self.roleId)
         assert result['status'] == 200
 
 
